0fc1fb254384db17a297818819547f36948a826c
ipa1987 <idris.patel1987@outlook.com>
2024-01-14 17:56:37 +0000

    Initial Setup 2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,36 +4,40 @@ module.exports = {
         "es2021": true
     },
     "extends": [
+        "airbnb",
+        "airbnb/hooks",
+        "airbnb-typescript",
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "prettier"
     ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
+    "overrides": [{
+        "env": {
+            "node": true
+        },
+        "files": [
+            ".eslintrc.{js,cjs}"
+        ],
+        "parserOptions": {
+            "sourceType": "script"
         }
-    ],
+    }],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
         },
         "ecmaVersion": "latest",
-        "sourceType": "module"
+        "sourceType": "module",
     },
     "plugins": [
         "@typescript-eslint",
-        "react"
+        "react",
+        "prettier"
     ],
     "rules": {
+        'react/react-in-jsx-scope': 0,
     },
     "settings": {
         "typescript": {}
