53b9e4a67199d0ca2b43ee5f2291614ebc3f38ae
ipa1987 <idris.patel1987@outlook.com>
2024-02-15 21:51:03 +0000

    folder restructure correction part 1

--- todo-api/dist/src/enums/Priority.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Priority = void 0;
-var Priority;
-(function (Priority) {
-    Priority["high"] = "high";
-    Priority["normal"] = "normal";
-    Priority["low"] = "low";
-})(Priority || (exports.Priority = Priority = {}));
